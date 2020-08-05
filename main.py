0cd4360b7996e02b725e5c34fef43828225ce068
urasha <43294860+urasha@users.noreply.github.com>
2020-08-05 21:54:58 +1000

    enemies added (not done full)

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ SIZE = (700, 600)
 RED = (200, 0, 0)
 GREY = (220, 220, 220)
 DARK_GREY = (120, 120, 120)
-GREEN = (0, 255, 0)
+GREEN = (100, 200, 100)
 FPS = 60
+
+SPAWN_ENEMY = pygame.USEREVENT + 1
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Dino')
@@ -20,10 +22,14 @@ clock = pygame.time.Clock()
 hero = {
     'x': 40,
     'y': 300,
-    'speed': 14
+    'speed': 15
 }
 
 is_jump = False
+
+enemy_pos = [[SIZE[0], 300]]
+pygame.time.set_timer(SPAWN_ENEMY, 1200)
+enemy_speed = 5
 
 
 def jump(y, speed):
@@ -34,7 +40,7 @@ def jump(y, speed):
         is_jump = False
 
         hero['y'] = 300
-        hero['speed'] = 14
+        hero['speed'] = 15
 
     return y
 
@@ -49,6 +55,8 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     is_jump = True
+            if event.type == SPAWN_ENEMY:
+                enemy_pos.append([SIZE[0], 300])
 
         if is_jump:
             hero['y'] = jump(hero['y'], hero['speed'])
@@ -56,9 +64,18 @@ def main():
 
         screen.fill(GREY)
 
+        for i in enemy_pos:
+            i[0] -= enemy_speed
+
+            # check enemy_pos and hero_pos
+            if i[0] in range(hero['x'], hero['x'] + 15) and i[1] in range(hero['y'], hero['y'] + 40):
+                pygame.time.delay(1000)
+                quit()
+
+            pygame.draw.rect(screen, GREEN, (i[0], i[1], 10, 40))
+
         pygame.draw.rect(screen, RED, (hero['x'], hero['y'], 15, 40))
         pygame.draw.rect(screen, DARK_GREY, (0, 340, SIZE[0], SIZE[1]))
-        pygame.draw.rect(screen, GREEN, (60, 300, 10, 40))  #test
 
         pygame.display.update()
         clock.tick(FPS)
